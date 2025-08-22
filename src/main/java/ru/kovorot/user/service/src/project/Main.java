bb68d896b6ff6f0201565eb619905bbded1a849e
Kovorot <kovorot0875@mail.ru>
2025-08-22 09:00:07 +0300

    Spring cloud added

--- src/main/java/ru/kovorot/user/service/src/project/Main.java
+++ src/main/java/ru/kovorot/user/service/src/project/Main.java
@@ -1,7 +1,10 @@
+package project;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Main {
 
     public static void main(String[] args) {
--- src/main/java/ru/kovorot/user/service/src/project/SwaggerConfig.java
+++ src/main/java/ru/kovorot/user/service/src/project/SwaggerConfig.java
@@ -1,3 +1,5 @@
+package project;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
